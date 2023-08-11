afbe9e5058d22c96576a6797a077e1197837dcc1
Edin237 <njomoeden@gmail.com>
2023-08-11 14:38:58 -0700

    all

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet reversted.
+ *
+ * Return: Always 0 (positive mindset)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
